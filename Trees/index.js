ea89f952f9fd42cd730ad3b3d95de56d95580bed
abhay <abhayforwork02@gmail.com>
2024-07-16 01:10:14 +0530

    Trees

--- Trees/index.js
+++ Trees/index.js
@@ -1,3 +1,5 @@
+const { text } = require("express");
+
 class Node {
     constructor(value) {
         this.value = value;
@@ -57,13 +59,73 @@ class BST {
             }
         }
     }
+    BFS() {                           // Breath First Search(BFS)
+        let current = this.root;
+        let queue = [];
+        let data= [];
+        queue.push(current);
+        while (queue.length) {
+            current = queue.shift();
+            data.push(current.value);
+            if (current.left) {
+                queue.push(current.left);
+            } if (current.right) {
+                queue.push(current.right);
+            }
+        }
+        return data;
+    }
+    DFS(node = this.root, data = []) {    // Depth First Search (DFS) Preorder.
+        if (node === null) {
+            return data;
+        }
+        //console.log(data);
+        data.push(node.value);
+        if (node.left) {
+            this.DFS(node.left,data);
+        } if (node.right) {
+            this.DFS(node.right,data);
+        }
+        return data;
+    }
+    DFSpostorder(node = this.root, data = []) {     // Depth First Search (DFS) Postorder
+        if (node === null) {
+            return data;
+        }
+        //console.log(data);
+        if (node.left) {
+            this.DFSpostorder(node.left,data);
+        } if (node.right) {
+            this.DFSpostorder(node.right,data);
+        }
+        data.push(node.value);
+        return data;
+    }
+    DFSinorder(node = this.root, data = []) {        // Depth First Search (DFS) Inorder.
+        if (node === null) {
+            return data;
+        }
+        console.log(data);
+        if (node.left) {
+            this.DFSinorder(node.left,data);
+        } 
+        data.push(node.value);
+
+        if (node.right) {
+            this.DFSinorder(node.right,data);
+        }
+        return data;
+    }
 }
 
-
 const tree = new  BST();
-tree.insert(8);
 tree.insert(4);
 tree.insert(7);
+tree.insert(2);
+tree.insert(1);
 tree.insert(8);
-tree.insert(10);
-console.log(tree.includes(22));
+// console.log(tree.includes(22));
+// console.log(tree.BFS());
+// console.log(tree.DFS());
+// console.log(tree.DFSpostorder());
+console.log(tree.DFSinorder());
